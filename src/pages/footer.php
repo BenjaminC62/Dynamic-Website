d6b3a90c29c5cc4c4ce098347ef6a39b8152ab3d
Pikablok <benjamincornet62130@gmail.com>
2024-09-22 16:15:01 +0200

    mise en place du logo ~ #12

--- src/pages/footer.php
+++ src/pages/footer.php
@@ -11,11 +11,24 @@
 <body>
 <footer>
     <div class="footer-main">
+
+        <div class="">
+            <img src="../imgs/Logo.png" class="logo">
+
+            <h3>About us</h3>
+            <span></span>
+
+
+            <h3>Contact us</h3>
+            <span>
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M18 24h-12c-1.104 0-2-.896-2-2v-20c0-1.104.896-2 2-2h12c1.104 0 2 .896 2 2v20c0 1.104-.896 2-2 2zm1-5.083h-14v3.083c0 .552.449 1 1 1h12c.552 0 1-.448 1-1v-3.083zm-7 3c-.553 0-1-.448-1-1s.447-1 1-1c.552 0 .999.448.999 1s-.447 1-.999 1zm7-17h-14v13h14v-13zm-1-3.917h-12c-.551 0-1 .449-1 1v1.917h14v-1.917c0-.551-.448-1-1-1zm-4.5 1.917h-3c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h3c.276 0 .5.224.5.5s-.224.5-.5.5z"/></svg>
+                <br>
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 21h-24v-18h24v18zm-23-16.477v15.477h22v-15.477l-10.999 10-11.001-10zm21.089-.523h-20.176l10.088 9.171 10.088-9.171z"/></svg>
+            </span>
+        </div>
+
+
         <ul class="footer-links">
-
-            
-
-
             <li><a href="../index.php">Accueil</a></li>
             <li><a href="./pages/about.php">À propos</a></li>
             <li><a href="contact.php">Contact</a></li>

eebdf0b4b3f415d16e69d60572afc99944500d16
MOREL Mathias <mathias_morel@ens.univ-artois.fr>
2024-09-25 18:22:40 +0200

    Mise en place du bouton (link) pour le mdp oublié

--- src/pages/login.php
+++ src/pages/login.php
@@ -11,6 +11,11 @@ include '../pages/menuNav.php';
         <input type="text" name="age"  placeholder="Votre âge" required><br>
         <input type="email" name="email" placeholder="Votre email" required><br>
         <input type="password" name="mdp" placeholder="Votre mot de passe" required><br>
+        <input type="password" name="mdp2" placeholder="Confirmez votre mot de passe" required>
+        <a href="#" class="forgot-password">Mot de passe oublié ?</a>
+        <br>
         <input type="submit" name="formsend">
     </form>
+
+
 
